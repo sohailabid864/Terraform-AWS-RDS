59a66076c731e055adcf8e8e74a66534a1bb1b72
sohailabid864 <150662198+sohailabid864@users.noreply.github.com>
2023-12-13 02:55:02 +0500

    Add files via upload

---
+++ terraform-rds-module/main.tf
@@ -0,0 +1,19 @@
+resource "aws_rds_cluster" "ecommerce-db" {
+  cluster_identifier = "ecommerce-db"
+  engine            = "aurora-mysql"
+  engine_version    = "5.7.mysql_aurora.2.07.9"
+  database_name     = "mydb"
+  master_username   = var.db_username
+  master_password   = var.db_password
+
+  
+}
+
+resource "aws_rds_cluster_instance" "ecommerce-db" {
+  count = 2
+  identifier = "ecommerce-db-${count.index}"
+  cluster_identifier = aws_rds_cluster.ecommerce-db.id
+  instance_class = "db.t2.small"
+  engine = "aurora-mysql"
+  publicly_accessible = false
+}
